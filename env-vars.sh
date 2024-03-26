9ca7d788ddd6149dfbe2bbbb02a8f30c172b7dae
Aruzhan Ilyazova <153256507+arILY092@users.noreply.github.com>
2024-03-26 18:18:49 +0600

    Add files via upload

---
+++ env-vars.sh
@@ -0,0 +1,16 @@
+export POSTGRES_DB=postgres
+export DATASOURCE_URL=jdbc:postgresql://db:5432/${POSTGRES_DB}
+export DATASOURCE_USERNAME=postgres
+export DATASOURCE_PASSWORD=postgres
+
+export JWT_SECRET=jdksladklsajdlkasdjlsadkasl
+
+export MINIO_BUCKET=data
+export MINIO_URL=htpp://localhost:9000
+export MINIO_ACCESS_KEY=minioadmin
+export MINIO_SECRET_KEY=minioadmin
+
+export ELASTICSEARCH_HOST=http://elasticsearch
+export ELASTICSEARCH_PORT=9200
+export ELASTICSEARCH_USERNAME=elastic
+export ELASTICSEARCH_PASSWORD=elastic
